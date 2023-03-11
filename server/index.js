25f4a4d17aa6203f835b79865ce420804ba1c04a
Saul Belisle <rbelisle1270@gmail.com>
2023-03-11 15:16:45 -0600

    updating server index static

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const request = require('request');
 const dotenv = require('dotenv');
 
 const port = 8888
+app.use(express.static(path.join(__dirname, '../build')));
 
 global.access_token = ''
 
@@ -11,7 +12,7 @@ dotenv.config()
 var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
 var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
-var spotify_redirect_uri = 'http://localhost:3000/auth/callback'
+var spotify_redirect_uri = 'https://saul-good-homie.github.io/spotify-api-v2/auth/callback'
 
 var generateRandomString = function (length) {
   var text = '';
